0817efaf16060a97c9b5d16c2ddc9428514000aa
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2026-05-24 19:06:47 +0100

    Nix

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "Utility for verifying the integrity of NuGet package attestations";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      pname = "AttestationIntegrity";
+      dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0;
+      dotnet-runtime = pkgs.dotnetCorePackages.runtime_10_0;
+      version = "0.1";
+      dotnetTool = dllOverride: toolName: toolVersion: hash:
+        pkgs.stdenvNoCC.mkDerivation rec {
+          name = toolName;
+          version = toolVersion;
+          nativeBuildInputs = [pkgs.makeWrapper];
+          src = pkgs.fetchNuGet {
+            pname = name;
+            version = version;
+            hash = hash;
+            installPhase = ''mkdir -p $out/bin && cp -r tools/net*/any/* $out/bin'';
+          };
+          installPhase = let
+            dll =
+              if isNull dllOverride
+              then name
+              else dllOverride;
+          in
+            # fsharp-analyzers requires the .NET SDK at runtime, so we use that instead of dotnet-runtime.
+            ''
+              runHook preInstall
+              mkdir -p "$out/lib"
+              cp -r ./bin/* "$out/lib"
+              makeWrapper "${dotnet-sdk}/bin/dotnet" "$out/bin/${name}" --set DOTNET_HOST_PATH "${dotnet-sdk}/bin/dotnet" --add-flags "$out/lib/${dll}.dll"
+              runHook postInstall
+            '';
+        };
+    in {
+      packages = let
+        deps = builtins.fromJSON (builtins.readFile ./nix/deps.json);
+      in {
+        fantomas = dotnetTool null "fantomas" (builtins.fromJSON (builtins.readFile ./.config/dotnet-tools.json)).tools.fantomas.version (builtins.head (builtins.filter (elem: elem.pname == "fantomas") deps)).hash;
+        fsharp-analyzers = dotnetTool "FSharp.Analyzers.Cli" "fsharp-analyzers" (builtins.fromJSON (builtins.readFile ./.config/dotnet-tools.json)).tools.fsharp-analyzers.version (builtins.head (builtins.filter (elem: elem.pname == "fsharp-analyzers") deps)).hash;
+        default = pkgs.buildDotnetModule {
+          inherit pname version dotnet-sdk dotnet-runtime;
+          name = "AttestationIntegrity";
+          src = ./.;
+          projectFile = "./RepoIntegrity/RepoIntegrity.fsproj";
+          testProjectFile = "./RepoIntegrity.Test/RepoIntegrity.Test.fsproj";
+          nugetDeps = ./nix/deps.json; # `nix build .#default.fetch-deps && ./result nix/deps.json`
+          doCheck = true;
+        };
+      };
+      devShell = pkgs.mkShell {
+        buildInputs = [dotnet-sdk];
+        DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+        # Force polling-based file watcher to avoid hangs in
+        # FileSystemWatcher.StartRaisingEvents on macOS (FSEvents/CoreFoundation
+        # path can deadlock under load when ASP.NET hosts created in tests
+        # initialise their JSON configuration providers).
+        DOTNET_USE_POLLING_FILE_WATCHER = "1";
+        packages = [
+          pkgs.alejandra
+          pkgs.lychee
+          pkgs.shellcheck
+          pkgs.xmlstarlet
+          pkgs.claude-code
+          pkgs.codex
+        ];
+      };
+    });
+}
